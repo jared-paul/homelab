1e0570b9f22812866d92e2990d278cccd8a6d634
jared-paul <jaredpaul9820@gmail.com>
2026-02-15 07:12:41 +0000

    set KUBECONFIG for k3s in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ echo "==> Installing K3s..."
 curl -sfL https://get.k3s.io | sh -
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 echo "==> Waiting for K3s to be ready..."
 sleep 10
 kubectl wait --for=condition=Ready node --all --timeout=120s
