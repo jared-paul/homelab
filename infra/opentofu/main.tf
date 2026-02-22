39a6a77406df8563b439ff7d7849faf9ddd282cb
jared-paul <jaredpaul9820@gmail.com>
2026-02-22 01:32:48 +0000

    add dns servers

--- infra/opentofu/main.tf
+++ infra/opentofu/main.tf
@@ -74,6 +74,10 @@ resource "proxmox_virtual_environment_vm" "cluster" {
   }
 
   initialization {
+    dns {
+      servers = ["1.1.1.1", "1.0.0.1"]
+    }
+
     ip_config {
       ipv4 {
         address = "${each.value.ip_address}/${var.cidr}"
